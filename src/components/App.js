7b80143b81761dbb7c60f9978579a10fcb57db39
Ananth Narayanan <ananth@chiselkraft.in>
2020-08-07 12:17:38 +0530

    UX-3 layouts / BaseLayout

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
 import React, { Component, Fragment } from "react";
 
+import BaseLayout from "./layouts";
+
 export class BatteryBuddyApp extends Component {
   render() {
-    return <Fragment></Fragment>;
+    return (
+      <Fragment>
+        <BaseLayout navigation="navigation" footer="footer">
+          section
+        </BaseLayout>
+      </Fragment>
+    );
   }
 }
 
