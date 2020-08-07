f11aeedf428d6949aa70a021ab73f283109ca697
Ananth Narayanan <ananth@chiselkraft.in>
2020-08-07 13:45:43 +0530

    DEV-4 Integration / Const - Typedef & Preferences
    ~ fn --link & unlink device
    ~ switch layout by link status

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React, { Component, Fragment } from "react";
 
+// Constants
+import * as Ty from "./constants/typedef";
+import * as Pr from "./constants/preferences";
+
+// Layout
 import BaseLayout from "./layouts";
 import CenterSection from "./layouts/sections/centerSection";
 import DefaultSection from "./layouts/sections/defaultSection";
 
+// Blocks
 import NavigationBlock from "./blocks/navigation";
 import FooterBlock from "./blocks/footer";
 import SidepanelBlock from "./blocks/sidepanel";
 
+// Pages
 import {
   DashboardPage,
   AnalyticsPage,
@@ -16,13 +23,87 @@ import {
 } from "./pages";
 
 class BatteryBuddyApp extends Component {
+  constructor(props) {
+    super(props);
+
+    // states
+    this.state = {
+      isLinked: Ty.LinkMode.CANBUS,
+    };
+
+    // bindings
+    this.link = this.link.bind(this);
+    this.unlink = this.unlink.bind(this);
+    this.checklink = this.checklink.bind(this);
+  }
+  componentDidMount() {}
+
+  link(mode) {
+    let status = this.state.isLinked;
+    // ...
+    // try to connect (Pr.LINK_RETRY) times
+    // ...
+
+    // success
+    status = mode;
+
+    // failed after retry
+    status = Ty.LinkMode.NONE;
+
+    this.setState({
+      isLinked: status,
+    });
+
+    return status;
+  }
+
+  unlink() {
+    // ...
+    if (this.state.isLinked) {
+      // ...
+      // unlink code
+      // ...
+    }
+    // on success
+    this.setState({
+      isLinked: Ty.LinkMode.NONE,
+    });
+  }
+
+  checklink() {
+    // ...
+    // check device link
+    // ...
+
+    // link success
+    // return 1
+
+    // link failed
+    this.setState({
+      isLinked: Ty.LinkMode.NONE,
+    });
+  }
+
   render() {
+    // cooking layout
+    let makeLayout;
+    if (!this.state.isLinked) {
+      // no link --show connection ui
+      makeLayout = <CenterSection>connect dialog</CenterSection>;
+    } else {
+      // linked --show app ui
+      makeLayout = (
+        <DefaultSection sidepanel={<SidepanelBlock />}>page</DefaultSection>
+      );
+    }
+
     return (
       <Fragment>
-        <BaseLayout navigation={<NavigationBlock />} footer={<FooterBlock />}>
-          <DefaultSection sidepanel={<SidepanelBlock />}>
-            <DashboardPage>dashboard</DashboardPage>
-          </DefaultSection>
+        <BaseLayout
+          navigation={<NavigationBlock showlinks={this.state.isLinked} />}
+          footer={<FooterBlock />}
+        >
+          {makeLayout}
         </BaseLayout>
       </Fragment>
     );
---
+++ src/components/constants/preferences.js
@@ -0,0 +1,5 @@
+export const CANBUS_ENABLE = 1;
+export const CLOUD_ENABLE = 1;
+export const LINK_RETRY = 3;
+
+export const SIDEPANEL_WIDTH = "290px";
