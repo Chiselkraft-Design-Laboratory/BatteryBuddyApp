8a0144827cdcf00f31817d0e1933e7226956c2e4
Ananth Narayanan <ananth@chiselkraft.in>
2020-08-07 16:23:07 +0530

    UX-10 icons & Navigation [complete]
    + Brand Logo & Icons
    + Conditional Rendering & Styling
    + State Management

--- src/components/blocks/navigation/index.js
+++ src/components/blocks/navigation/index.js
@@ -1,24 +1,63 @@
 import React from "react";
-import { Toolbar, makeStyles } from "@material-ui/core";
+import { Toolbar, Box, makeStyles } from "@material-ui/core";
 
 import * as Url from "../../constants/routes";
+import { SIDEPANEL_WIDTH } from "../../constants/preferences";
+import {
+  BrandIcon,
+  DashboardIcon,
+  AnalyticsIcon,
+  DiagnosticsIcon,
+  SettingsIcon,
+} from "../../icons";
 
 import Navlink from "./navlink";
 
 const useStyle = makeStyles((theme) => ({
   root: {
-    padding: 0,
+    padding: theme.spacing(1, 0),
+    alignItems: "flex-end",
+  },
+  brand: {
+    width: SIDEPANEL_WIDTH,
+  },
+  links: {
+    flexGrow: 1,
   },
 }));
 const NavigationBlock = ({ active, showlinks }) => {
   const classes = useStyle();
+
   return (
     <Toolbar className={classes.root}>
-      [Brand]
-      <Navlink text="home" url={Url.DASHBOARD} />
-      <Navlink text="analytics" url={Url.ANALYTICS} />
-      <Navlink text="diagnostics" url={Url.DIAGNOSTICS} isActive />
-      <Navlink text="settings" url={Url.SETTINGS} />
+      <Box className={classes.brand}>
+        <BrandIcon size={80} />
+      </Box>
+
+      {showlinks ? (
+        <Box className={classes.links}>
+          <Navlink
+            icon={<DashboardIcon size={24} />}
+            label="home"
+            url={Url.DASHBOARD}
+          />
+          <Navlink
+            icon={<AnalyticsIcon size={24} />}
+            label="analytics"
+            url={Url.ANALYTICS}
+          />
+          <Navlink
+            icon={<DiagnosticsIcon size={24} />}
+            label="diagnostics"
+            url={Url.DIAGNOSTICS}
+          />
+          <Navlink
+            icon={<SettingsIcon size={24} />}
+            label="settings"
+            url={Url.SETTINGS}
+          />
+        </Box>
+      ) : null}
     </Toolbar>
   );
 };
