a8de046bf7fe720343b48697957263a9365dc5c6
Ananth Narayanan <ananth@chiselkraft.in>
2020-08-07 13:18:04 +0530

    UX-8 Pages / ...

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,21 @@ import NavigationBlock from "./blocks/navigation";
 import FooterBlock from "./blocks/footer";
 import SidepanelBlock from "./blocks/sidepanel";
 
-export class BatteryBuddyApp extends Component {
+import {
+  DashboardPage,
+  AnalyticsPage,
+  DiagnosticsPage,
+  SettingsPage,
+} from "./pages";
+
+class BatteryBuddyApp extends Component {
   render() {
     return (
       <Fragment>
         <BaseLayout navigation={<NavigationBlock />} footer={<FooterBlock />}>
-          <DefaultSection sidepanel={<SidepanelBlock />}>page</DefaultSection>
+          <DefaultSection sidepanel={<SidepanelBlock />}>
+            <DashboardPage>dashboard</DashboardPage>
+          </DefaultSection>
         </BaseLayout>
       </Fragment>
     );
---
+++ src/components/pages/index.js
@@ -0,0 +1,6 @@
+import DashboardPage from "./dashboard";
+import AnalyticsPage from "./analytics";
+import DiagnosticsPage from "./diagnostics";
+import SettingsPage from "./settings";
+
+export { DashboardPage, AnalyticsPage, DiagnosticsPage, SettingsPage };
