a3652d51c34c45e2634b5c3bc69a0f974708216b
Ananth Narayanan <ananth@chiselkraft.in>
2020-08-27 12:08:22 +0530

    D1 Structure

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class BatteryBuddyApp extends Component {
+  render() {
+    return <div>Hello world!</div>;
+  }
+}
+
+export default BatteryBuddyApp;
