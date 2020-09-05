9803d87d1ed0672f7b1db0f92c33dd135f13f94b
Ananth Narayanan <ananth@chiselkraft.in>
2020-09-05 21:41:03 +0530

    U10 R1 revision & improvements
    ~ customizable spinner
    ~ enable/disable support
    ~ layout improvements

--- src/components/canvas/connectwizard/index.js
+++ src/components/canvas/connectwizard/index.js
@@ -1,18 +1,9 @@
 import React from "react";
-import {
-  makeStyles,
-  Box,
-  Fab,
-  Paper,
-  Grid,
-  Button,
-  Typography,
-  CircularProgress,
-} from "@material-ui/core";
+import { makeStyles, Paper, Grid } from "@material-ui/core";
 
 import withCanvas from "../withCanvas";
-import { linkMode } from "../../constants/typedef";
-import { CanBusIcon, CloudIcon } from "../../assets";
+import ConnectProgress from "./connectProgress";
+import ConnectDialog from "./connectDialog";
 
 const useStyles = (dense) =>
   makeStyles(
@@ -26,70 +17,9 @@ const useStyles = (dense) =>
         height: dense ? "100%" : 350,
         background: dense && theme.palette.D2,
         display: "flex",
-        // alignContent: "center",
         flexDirection: "column",
         alignItems: "stretch",
         justifyContent: "center",
-      },
-      title: {
-        padding: theme.spacing(3, 2),
-      },
-      contentWrap: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: theme.spacing(2, 4),
-        margin: theme.spacing(4, 0),
-      },
-      button: {
-        width: 128,
-        height: 128,
-        padding: theme.spacing(2),
-        margin: theme.spacing(1, 2),
-        borderRadius: theme.spacing(2),
-      },
-      btncaption: {
-        marginTop: theme.spacing(1),
-      },
-      default: {
-        background: `linear-gradient(-45deg, ${theme.palette.L1} 30%, ${theme.palette.L2} 100%)`,
-        color: theme.palette.D3,
-        fill: theme.palette.D3,
-        transition: "all 0.2s ease-in",
-        "&:hover": {
-          background: theme.palette.G1,
-          fill: theme.palette.D2,
-          color: theme.palette.D2,
-          transform: "scale(1.05)",
-          transition: "all 0.2s ease-in",
-        },
-        disabled: {
-          background: theme.palette.D3,
-          color: theme.palette.D4,
-          fill: theme.palette.D4,
-        },
-      },
-      spinnerWrap: {
-        margin: theme.spacing(1),
-        position: "relative",
-      },
-      spinner: {
-        position: "absolute",
-        top: -48,
-        left: -48,
-        color: theme.palette.T1,
-        zIndex: 1,
-      },
-      spinnerIcon: {
-        position: "absolute",
-        width: 96,
-        height: 96,
-        top: -48,
-        left: -48,
-        boxShadow: "none",
-        background: "inherit",
-        fill: theme.palette.T1,
       },
     }),
 
@@ -115,83 +45,10 @@ const ConnectWizard = ({ canvas, connect }) => {
         {progress ? (
           <ConnectProgress mode={progress} canvas={canvas} />
         ) : (
-          <ConnectOption connect={handleConnect} canvas={canvas} />
+          <ConnectDialog connect={handleConnect} canvas={canvas} />
         )}
       </Paper>
     </Grid>
-  );
-};
-
-const ConnectOption = ({ canvas, connect }) => {
-  const cl = useStyles(canvas.dense)();
-  return (
-    <React.Fragment>
-      <Typography variant="h6" align="center" classes={{ root: cl.title }}>
-        select a connection
-      </Typography>
-      <Box classes={{ root: cl.contentWrap }}>
-        {/* cloud button */}
-        <Button
-          disabled
-          classes={{ root: cl.button }}
-          className={cl.disabled}
-          onClick={() => connect(linkMode.CLOUD)}
-        >
-          <CloudIcon size={48} />
-          <Typography variant="caption">connect via</Typography>
-          <Typography variant="body1">
-            <strong>CLOUD</strong>
-          </Typography>
-        </Button>
-        {/* canbus button */}
-        <Button
-          classes={{ root: cl.button }}
-          className={cl.default}
-          onClick={() => connect(linkMode.CANBUS)}
-        >
-          <CanBusIcon size={48} />
-          <Typography variant="caption" classes={{ root: cl.btncaption }}>
-            connect via
-          </Typography>
-          <Typography variant="body1">
-            <strong>CANBUS</strong>
-          </Typography>
-        </Button>
-      </Box>
-    </React.Fragment>
-  );
-};
-
-const ConnectProgress = ({ canvas, mode }) => {
-  const cl = useStyles(canvas.dense)();
-  const spinnerSize = 96;
-  const iconSize = 48;
-
-  return (
-    <React.Fragment>
-      <Box classes={{ root: cl.contentWrap }}>
-        <div className={cl.spinnerWrap}>
-          {mode && (
-            <Fab elevation={0} disableRipple classes={{ root: cl.spinnerIcon }}>
-              {mode === linkMode.CLOUD ? (
-                <CloudIcon size={iconSize} />
-              ) : (
-                <CanBusIcon size={iconSize} />
-              )}
-            </Fab>
-          )}
-          <CircularProgress
-            size={spinnerSize}
-            thickness={2}
-            classes={{ root: cl.spinner }}
-          />
-        </div>
-      </Box>
-      <Typography variant="caption" align="center" classes={{ root: cl.title }}>
-        connecting via
-        <strong>{mode === linkMode.CLOUD ? " CLOUD" : " CANBUS"}</strong>
-      </Typography>
-    </React.Fragment>
   );
 };
 
--- src/components/constants/preferences.js
+++ src/components/constants/preferences.js
@@ -1,2 +1,10 @@
 export const UiBreakpoint = "sm";
 export const sidebarWidth = 290;
+
+export const connectWizardOptions = {
+  enableCANBUS: true,
+  enableCLOUD: false,
+  spinnerSize: 128,
+  spinnerThickness: 0.6,
+  spinnerIconSize: 40,
+};
